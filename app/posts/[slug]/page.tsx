b393539da73a47857c09f4b95f610127a286dae3
Alex Lux <alexlux@live.com>
2023-11-25 12:38:38 -0800

    test

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,32 +1,26 @@
 import fs from "fs";
-
-// **** npm i markdown-to-jsx ****
 import Markdown from "markdown-to-jsx";
 import matter from "gray-matter";
 import getPostMetadata from "@/components/getPostMetadata";
 
 const getPostContent = (slug: string) => {
-  const folder = "public/posts/";
+  const folder = "posts/";
   const files = `${folder}${slug}.md`;
   const content = fs.readFileSync(files, "utf8");
   const matterResult = matter(content);
-  console.log(matterResult);
   return matterResult;
 };
 
 export const generateStaticParams = async () => {
   const posts = getPostMetadata();
   return posts.map((post) => ({
-    params: {
-      slug: post.slug,
-    },
+    slug: post.slug,
   }));
 };
 
 const PostPage = (props: any) => {
   const slug = props.params.slug;
   const post = getPostContent(slug);
-  console.log(post);
 
   return (
     <div>
--- components/getPostMetadata.ts
+++ components/getPostMetadata.ts
@@ -3,20 +3,20 @@ import fs from "fs";
 import matter from "gray-matter";
 
 const getPostMetadata = (): PostMetadata[] => {
-  const folder = "public/posts/";
+  const folder = "posts/";
   const files = fs.readdirSync(folder);
   const markdownPosts = files.filter((file) => file.endsWith(".md"));
   // const slugs = markdownPosts.map((file) => file.replace(".md", ""));
 
   const posts = markdownPosts.map((fileName) => {
-    const fileContetns = fs.readFileSync(`${folder}${fileName}`, "utf8");
-    const matterResult = matter(fileContetns);
+    const fileContents = fs.readFileSync(`posts/${fileName}`, "utf8");
+    const matterResult = matter(fileContents);
     return {
       title: matterResult.data.title,
       date: matterResult.data.date,
-      slug: fileName.replace(".md", ""),
       subtitle: matterResult.data.subtitle,
       author: matterResult.data.author,
+      slug: fileName.replace(".md", ""),
     };
   });
 
